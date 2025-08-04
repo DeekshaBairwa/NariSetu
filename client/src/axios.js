d77afbe65f7ac159a4458f854f3d6352b7c64e65
Deeksha Bairwa <deekshadhawan26@gmail.com>
2025-08-04 09:22:19 +0530

    Fix: dynamic axios baseURL for deployment

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,8 +1,12 @@
 // src/axios.js
 import axios from 'axios';
 
+const isProd = import.meta.env.PROD;
+
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: isProd
+    ? 'https://narisetu-backend.onrender.com'
+    : 'http://localhost:5000',
   withCredentials: true
 });
 
